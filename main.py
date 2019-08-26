659c064a4bc6ad3bf8876c9e86da0fee1046412b
Chance <cembreyfarquhar@gmail.com>
2019-08-26 14:53:19 -0500

    basics done - working on main function

---
+++ main.py
@@ -0,0 +1,65 @@
+import numpy as np
+import random as r
+import sys
+
+class MontyHall:
+    def __init__(self):
+        self.doors = (1, 2, 3)
+        self.prize_door = r.randint(1, 3)
+        self.goat_doors = [door for door in self.doors if door is not self.prize_door]
+        self.first_choice = None
+        self.goat_shown = r.choice(self.goat_doors)
+        self.final_choice = None
+        # print(f"You choose door {self.first_choice}")
+        # print(f"Door {self.goat_shown} is shown. It's a goat! Do you want to keep your choice or change?")
+    
+    def make_first_choice(self):
+        inp = input("Which door shall you choose? (1, 2, or 3)")
+        self.first_choice = int(inp)
+
+    def keep_choice(self):
+        self.final_choice = self.first_choice        
+
+    def change_choice(self):
+        for door in self.doors:
+            if door is not self.first_choice and door is not self.goat_shown:
+                self.final_choice = door
+
+    def did_win(self):
+        if self.final_choice is self.prize_door:
+            return True
+        else:
+            return False
+
+    def sim_game(self, keep_or_not):
+        self.first_choice = r.randint(1, 3)
+        if keep_or_not:
+            self.keep_choice()
+        else:
+            self.change_choice()
+        if self.did_win():
+            return True
+        else:
+            return False    
+
+
+
+def simulation():
+    mh = MontyHall()
+    mh.sim_game(True)
+    inp = input("How many iterations do you want to run?")
+    iterations = int(inp)
+    keep_choice_wins = 0
+    change_choice_wins = 0
+    for i in range(iterations):
+        mh = MontyHall()
+        if mh.sim_game(True) is True:
+            keep_choice_wins += 1
+        else:
+            change_choice_wins += 1
+    print(f"Keeping the first choice resulted in {keep_choice_wins} wins out of {iterations} games, or {keep_choice_wins / iterations * 100}% of the time")
+    print(f"Changing the first choice resulted in {change_choice_wins} wins out of {iterations} games, or {change_choice_wins / iterations * 100}% of the time")
+
+
+    
+
